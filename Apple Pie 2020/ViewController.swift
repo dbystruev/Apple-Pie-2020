b71e427cbb3eebafc81ad1ad43f17b41b307228f
Denis Bystruev <dbystruev@me.com>
2020-08-10 16:35:07 +0300

    Connected UI to outlets and actions

--- Apple Pie 2020/ViewController.swift
+++ Apple Pie 2020/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - IB Outlets
+    @IBOutlet weak var treeImageView: UIImageView!
+    @IBOutlet var letterButtons: [UIButton]!
+    @IBOutlet weak var coorectWordLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    // MARK: - IB Actions
+    
+    @IBAction func letterButtonPressed(_ sender: UIButton) {
+        sender.isEnabled = false
+    }
 }
 
